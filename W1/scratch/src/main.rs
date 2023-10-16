e44bd237c99cfd1b8efd73259dc9a4b0304a8e78
zf <tse02030@gmail.com>
2023-10-17 04:30:13 +1100

    w5

--- W1/scratch/src/main.rs
+++ W1/scratch/src/main.rs
@@ -1,10 +1,13 @@
-
-
-fn main() {
-
-    match [1,2,3] {
-        [_, _, _] => println!("three"),
-    }
-
+fn insert_value<'vec_lifetime>(my_vec: &'vec_lifetime mut Vec<&'vec_lifetime i32>, value: &'vec_lifetime i32) {
+    my_vec.push(value)
 }
-
+fn main(){
+    let mut my_vec = vec![];
+    let val1 = 1;
+    let val2 = 2;
+    
+    insert_value(&mut my_vec, &val1);
+    insert_value(&mut my_vec, &val2);
+    
+    println!("{my_vec:?}");
+}
